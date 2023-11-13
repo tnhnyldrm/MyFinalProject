db5ed99299ccf79dca9a1c8a83f35682a21d52db
tnhnyldrm <46500254+tnhnyldrm@users.noreply.github.com>
2023-11-13 09:08:56 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,9 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+foreach (var item in productManager.GetAll())
+{
+    Console.WriteLine(item.ProductName);
+}

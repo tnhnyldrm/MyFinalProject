a9257d0c9ff2384e0c33e8f2241672f2cdfff7ef
tnhnyldrm <46500254+tnhnyldrm@users.noreply.github.com>
2023-11-19 18:21:34 +0300

    10. gün video sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Concrete;
+using Core.Utilities.Results;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
@@ -11,10 +12,21 @@ static void ProductTest()
 {
     ProductManager productManager = new ProductManager(new EfProductDal());
 
-    foreach (var product in productManager.GetProductDetails())
+    var result = productManager.GetProductDetails();
+
+    if (result.Success==true)
     {
-        Console.WriteLine(product.CategoryName + "/" + product.CategoryName);
+        foreach (var product in result.Data)
+        {
+            Console.WriteLine(product.ProductName + "/" +product.CategoryName);
+        }
+
     }
+    else
+    {
+        Console.WriteLine(result.Message);
+    }
+
 }
 
 static void CategoryTest()
